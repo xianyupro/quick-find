3f30eb0c87e506fa5aed221ae97356bcefa7512f
肖鹏 <837249941@qq.com>
2020-08-28 11:06:47 +0800

    重新添加查询功能

--- QuickFind/QuickForm.cs
+++ QuickFind/QuickForm.cs
@@ -218,7 +218,7 @@ namespace QuickFind
         {
             InitializeComponent();
 
-            //boLoSearch = new BoLoSearch.BoLoSearch();
+            boLoSearch = new BoLoSearch.BoLoSearch();
 
             SetStartup(_settings.LaunchOnStartup);
 
